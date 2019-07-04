d849a18bc293c2dd4672ebce64734f9b1be9bd6c
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-07-05 04:47:09 +0800

    Problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -2586,3 +2586,26 @@ def is_palindrome(str_1, str_2, m, n):
 
     return 1 + min(is_palindrome(str_1, str_2, m-1, n), is_palindrome(str_1, str_2, m, n-1))
 
+
+"""You are given a 2-d matrix where each cell represents number of coins in that cell. Assuming we start at
+matrix[0][0], and can only move right or down, find the maximum number of coins you can collect by the bottom
+right corner. For example, in this matrix
+0 3 1 1
+2 0 0 4
+1 5 3 1
+The most we can collect is 0 + 2 + 1 + 5 + 3 + 1 = 12 coins."""
+
+
+def most_coins(matrix, m=0, n=0):
+    if m == len(matrix) - 1 and n == len(matrix[0]) - 1:
+        return matrix[m][n]
+
+    elif m == len(matrix) - 1:
+        return matrix[m][n] + most_coins(matrix, m, n+1)
+
+    elif n == len(matrix[0]) - 1:
+        return matrix[m][n] + most_coins(matrix, m+1, n)
+
+    return matrix[m][n] + max(most_coins(matrix, m, n+1), most_coins(matrix, m+1, n))
+
+
