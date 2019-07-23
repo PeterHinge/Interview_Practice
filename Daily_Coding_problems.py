0bc134818a8a0ff10871bba847b1255f3c57269e
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-07-23 12:41:38 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -3017,3 +3017,22 @@ def min_coins(n):
     coins += n
 
     return coins
+
+
+"""Given an array of integers in which two elements appear exactly once and all other elements appear exactly twice,
+find the two elements that appear only once. For example, given the array [2, 4, 6, 8, 10, 2, 6, 10], return 4 and 8.
+The order does not matter. Follow-up: Can you do this in linear time and constant space?"""
+
+
+def two_elements(array):
+    array.sort()
+    index = 0
+    output = None
+    while True:
+        if index == len(array) - 1 or array[index] != array[index+1]:
+            if output:
+                return output, array[index]
+            output = array[index]
+            index += 1
+        else:
+            index += 2
