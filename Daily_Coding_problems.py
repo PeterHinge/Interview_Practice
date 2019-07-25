1d5b88d199dde8dd92cf989e2868d5c116d0853f
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-07-25 20:12:26 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -3060,3 +3060,29 @@ class Stack:
 
     def push(self, item, stack_number):
         self.list[stack_number].append(item)
+
+        
+"""Given a pivot x, and a list lst, partition the list into three parts.
+The first part contains all elements in lst that are less than x
+The second part contains all elements in lst that are equal to x
+The third part contains all elements in lst that are larger than x
+Ordering within a part can be arbitrary.
+For example, given x = 10 and lst = [9, 12, 3, 5, 14, 10, 10], one partition may be [9, 3, 5, 10, 10, 12, 14]"""
+
+
+def pivot(lst, x):
+    partition = []
+    small_end_index = 0
+    holder = 0
+    for element in lst:
+        if element < x:
+            partition.insert(0, element)
+            small_end_index += 1
+        elif element > x:
+            partition.append(element)
+        else:
+            holder += 1
+    for i in range(holder):
+        partition.insert(small_end_index, x)
+
+    return partition
