d63360af022377e523ad3342f3d16a4fb2d14692
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-08-03 18:28:09 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -3332,3 +3332,28 @@ def fill_change(matrix, pixel, color):
 def get_adjacent(x, y):
     adjacent = [[0, 1], [1, 0], [0, -1], [-1, 0]]
     return [[x + i[0], y + i[1]] for i in adjacent]
+
+
+"""You are given n numbers as well as n probabilities that sum up to 1. Write a function to generate one of the numbers
+with its corresponding probability. For example, given the numbers [1, 2, 3, 4] and probabilities [0.1, 0.5, 0.2, 0.2],
+your function should return 1 10% of the time, 2 50% of the time, and 3 and 4 20% of the time.
+You can generate random numbers between 0 and 1 uniformly."""
+import random
+
+
+def corresponding_probability(nums, probs):
+    choice = random.uniform(0, 1)
+
+    total = 0
+
+    options = []
+
+    for val in probs:
+        total += val
+        options.append(total)
+
+    assert total == 1
+
+    for i, val in enumerate(options):
+        if choice <= val:
+            return nums[i]
