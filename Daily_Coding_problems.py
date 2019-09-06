279fc519ec82e5947d937825ec5b3359550ca734
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-09-06 21:55:08 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -3829,3 +3829,26 @@ class Solution:
         return sum / 10000
 
 # She should pick first game (conditional probability)
+
+
+"""Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1]."""
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        start = 0
+        end = len(nums) - 1
+        while start < end:
+            if nums[start] + nums[end] == target:
+                return [start, end]
+            elif nums[start] + nums[end] < target:
+                start += 1
+            else:
+                end -= 1
+        return None
+    
