173bc52552e219fc4acb73d6df46c283621ad0c4
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-09-13 20:19:48 +0200

    problem aaded

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -3929,3 +3929,24 @@ class Solution:
                 return i
 
         return -1
+
+    
+"""You are given an array of nonnegative integers. Let's say you start at the beginning of the array and are trying
+to advance to the end. You can advance at most, the number of steps that you're currently on. Determine whether
+you can get to the end of the array. For example, given the array [1, 3, 1, 2, 0, 1], we can go from indices
+0 -> 1 -> 3 -> 5, so return true. Given the array [1, 2, 1, 0, 0], we can't reach the end, so return false."""
+
+
+def can_reach_end(array):
+    if not array:
+        return []
+
+    if array[0] >= len(array) - 1:
+        return True
+
+    else:
+        for i in range(array[0]):
+            if can_reach_end(array[i+1:]):
+                return True
+
+    return False
