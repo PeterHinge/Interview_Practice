228767a36aba3d1246a10a9b795173934f4bcc08
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-09-19 13:34:09 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4018,3 +4018,24 @@ def rotate(array, k):
             array[-1-j], array[-2-j] = array[-2-j], array[-1-j]
 
     return array
+
+
+"""Given a set of distinct positive integers, find the largest subset such that every pair of elements
+in the subset (i, j) satisfies either i % j = 0 or j % i = 0. For example, given the set [3, 5, 10, 20, 21],
+you should return [5, 10, 20]. Given [1, 3, 6, 24], return [1, 3, 6, 24]."""
+
+
+def largest_modular_subpair(array):
+    hm = {}
+    ls = []
+
+    for num in array:
+        hm.setdefault(num, [])
+        for element in hm:
+            if num % element == 0:
+                hm[element].append(num)
+                if len(hm[element]) > len(ls):
+                    ls = hm[element]
+
+    return ls
+
