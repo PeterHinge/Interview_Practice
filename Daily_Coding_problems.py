5862e03c7c713792c3545fab6894f775e53ff84b
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-09-22 19:31:10 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4057,3 +4057,30 @@ def real_line(array):
             end = point[0]
 
     return [start, end]
+
+
+"""You are given an array of arrays of integers, where each array corresponds to a row in a triangle of numbers.
+For example, [[1], [2, 3], [1, 5, 1]] represents the triangle:
+  1
+ 2 3
+1 5 1
+We define a path in the triangle to start at the top and go down one row at a time to an adjacent value,
+eventually ending with an entry on the bottom row. For example, 1 -> 3 -> 5. The weight of the path is the
+sum of the entries. Write a program that returns the weight of the maximum weight path."""
+
+
+def max_weight_path(array):
+    if not array:
+        return 0
+
+    current_weight = array[-1]
+    for i in range(len(array) - 1):
+        next_level = []
+        for j in range(len(current_weight) - 1):
+            if array[-2-i][j] + current_weight[j] > array[-2-i][j] + current_weight[j+1]:
+                next_level.append(array[-2-i][j] + current_weight[j])
+            else:
+                next_level.append(array[-2-i][j] + current_weight[j+1])
+        current_weight = next_level
+
+    return current_weight[0]
