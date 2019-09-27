464bfdf9070d3b0a0cba5d48d21140424f0bfeb3
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-09-27 12:41:59 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4141,3 +4141,18 @@ class BinaryTree:
 
     def count(self):
         print(len(self.notes))
+
+        
+"""A permutation can be specified by an array P, where P[i] represents the location of the element at i
+in the permutation. For example, [2, 1, 0] represents the permutation where elements at the index 0 and 2 are swapped.
+Given an array and a permutation, apply the permutation to the array. For example, given the array ["a", "b", "c"]
+and the permutation [2, 1, 0], return ["c", "b", "a"]."""
+
+
+def permutation_swap(array, perm):
+    new_array = [i for i in range(len(array))]
+
+    for i, num in enumerate(perm):
+        new_array[i] = array[num]
+
+    return new_array
