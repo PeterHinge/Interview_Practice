efaa45b76de3b4fd9c900e9971b2148212cc7e59
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-10-06 20:41:23 +0200

    problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4291,3 +4291,32 @@ def logest_1_binary(n):
         ones = counter
     
     return ones
+
+
+"""Given a number in Roman numeral format, convert it to decimal. The values of Roman numerals are as follows:
+    'M': 1000,
+    'D': 500,
+    'C': 100,
+    'L': 50,
+    'X': 10,
+    'V': 5,
+    'I': 1
+In addition, note that the Roman numeral system uses subtractive notation for numbers such as IV and XL.
+For the input XIV, for instance, you should return 14."""
+
+
+def roman_to_num(string):
+    lib = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    num = 0
+
+    for i, letter in enumerate(string):
+        if i <= len(string) - 2 and lib[letter] < lib[string[i+1]]:
+            continue
+        else:
+            if i != 0 and lib[letter] > lib[string[i - 1]]:
+                num += lib[letter] - lib[string[i - 1]]
+            else:
+                num += lib[letter]
+
+    return num
