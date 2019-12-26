a900c238fc3215e2ba708e3f4466ca2b50a3ccb2
Peter <46064696+PeterHinge@users.noreply.github.com>
2019-12-26 16:19:44 +0100

    Update Daily_Coding_problems.py

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4400,3 +4400,40 @@ def pythogorean_triplet(a, b, c):
         return a** 2 + c ** 2 == b ** 2
     else:
         return a ** 2 + b ** 2 == c ** 2
+
+     
+"""A classroom consists of N students, whose friendships can be represented in an adjacency list.
+For example, the following descibes a situation where 0 is friends with 1 and 2, 3 is friends with 6, and so on.
+{0: [1, 2],
+ 1: [0, 5],
+ 2: [0],
+ 3: [6],
+ 4: [],
+ 5: [1],
+ 6: [3]}
+Each student can be placed in a friend group, which can be defined as the transitive closure of that student's
+friendship relations. In other words, this is the smallest set such that no student in the group has any friends
+outside this group. For the example above, the friend groups would be {0, 1, 2, 5}, {3, 6}, {4}.
+Given a friendship list such as the one above, determine the number of friend groups in the class."""
+
+
+def num_of_groups(friendships):
+    groups = 0
+    holder = set()
+
+    def helper(value):
+        for val in value:
+            if val not in holder:
+                holder.add(val)
+                helper(friendships[val])
+
+    for key, value in friendships.items():
+        if value == []:
+            groups += 1
+        else:
+            if key not in holder:
+                holder.add(key)
+                groups += 1
+                helper(value)
+
+    return groups
