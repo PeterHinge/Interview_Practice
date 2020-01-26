072d468d6550d08e577c461226f7c60230769266
Peter <46064696+PeterHinge@users.noreply.github.com>
2020-01-26 11:31:22 +0100

    Update Daily_Coding_problems.py

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4569,3 +4569,24 @@ def add_2_binary_trees(root1, root2):
     tree_root = (add_2_binary_nodes(root1, root2))
 
     return tree_root
+
+
+"""Consider the following scenario: there are N mice and N holes placed at integer points along a line.
+Given this, find a method that maps mice to holes such that the largest number of steps any mouse takes is minimized.
+Each move consists of moving one mouse one unit to the left or right, and only one mouse can fit inside each hole.
+For example, suppose the mice are positioned at [1, 4, 9, 15], and the holes are located at [10, -5, 0, 16]. In this
+case, the best pairing would require us to send the mouse at 1 to the hole at -5, so our function should return 6."""
+
+
+def minimize_mice_to_holes(mize, holes):
+    mize.sort()
+    holes.sort()
+
+    max_diff = 0
+
+    for i in range(len(mize)):
+        if abs(mize[i] - holes[i]) > max_diff:
+            max_diff = abs(mize[i] - holes[i])
+
+    return max_diff
+
