607ddba506dcb92e6ff0552e3e3ed505d66f25c0
Peter <46064696+PeterHinge@users.noreply.github.com>
2020-03-15 19:53:23 +0100

    Problem added

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4698,4 +4698,31 @@ def strobogrammatic_numbers(n):
     return numbers
 
 
+"""You are writing an AI for a 2D map game. You are somewhere in a 2D grid, and there are coins strewn about over the map.
+Given the position of all the coins and your current position, find the closest coin to you in terms of Manhattan distance.
+That is, you can move around up, down, left, and right, but not diagonally. If there are multiple possible closest coins,
+return any of them.For example, given the following map, where you are x, coins are o, and empty spaces are . (top left is 0, 0):
+---------------------
+| . | . | x | . | o |
+---------------------
+| o | . | . | . | . |
+---------------------
+| o | . | . | . | o |
+---------------------
+| . | . | o | . | . |
+---------------------
+return (0, 4), since that coin is closest. This map would be represented in our question as: Our position: (0, 2)
+Coins: [(0, 4), (1, 0), (2, 0), (3, 2)]"""
+
+
+def closet_coin(our_pos, coins_pos):
+    closet = [(), None]
+
+    for pos in coins_pos:
+        dist = abs(pos[0] - our_pos[0]) + abs(pos[1] - our_pos[1])
+        if not closet[1] or dist < closet[1]:
+            closet = [pos, dist]
+
+    return closet[0]
+
 
