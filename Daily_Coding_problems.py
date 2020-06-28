559f28bdfb831f17c9079e5a3c57a63f6f1fdcc4
Peter <46064696+PeterHinge@users.noreply.github.com>
2020-06-28 21:32:40 +0200

    Update Daily_Coding_problems.py

--- Daily_Coding_problems.py
+++ Daily_Coding_problems.py
@@ -4902,3 +4902,22 @@ def most_people_in_building(data):
             current_people -= data[i]['count']
 
     return most_people_time
+
+
+"""What does the below code snippet print out? How can we fix the anonymous functions to behave as we'd expect?
+
+functions = []
+for i in range(10):
+    functions.append(lambda : i)
+
+for f in functions:
+    print(f())
+
+"""
+
+functions = []
+for i in range(10):
+    functions.append(lambda i=i: i)
+
+for f in functions:
+    print(f())
